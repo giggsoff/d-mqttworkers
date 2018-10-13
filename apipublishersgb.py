4b830c3b62056e029d1bb0962e9e317eda04494e
Peter Fedchenkov <giggsoff@gmail.com>
2018-10-13 21:22:59 +0300

    sgb publisher

--- apipublishersgb.py
+++ apipublishersgb.py
@@ -1,9 +1,26 @@
-
 import ssl
 import sys
 import time
 import paho.mqtt.client
 import paho.mqtt.publish
+from flask import Flask
+import thread
+import json
+
+level = 0.
+timer = 3.
+maxval = 100.
+temp = 20.
+id = 12345
+
+app = Flask(__name__)
+
+@app.route('/level/<new_level>')
+def setlevel(new_level):
+	global level
+	oldlevel = str(level)
+	level = new_level
+	return "Old level: "+oldlevel+"\n"
 
 def on_connect(client, userdata, flags, rc):
 	print('connected')
@@ -11,9 +28,10 @@ def on_connect(client, userdata, flags, rc):
 def main():
 	i = 1
 	while True:
+		data = {'i':id,'l':level,'m':maxval,'t':temp,'d':int(round(time.time()))}
 		paho.mqtt.publish.single(
 			topic='topic1',
-			payload='message'+str(i),
+			payload=json.dumps(data),
 			qos=2,
 			hostname='127.0.0.1',
 			port=1884,
@@ -27,8 +45,13 @@ def main():
 			#	'tls_version': ssl.PROTOCOL_TLSv1_2
 			#}
 		)
-		time.sleep(3)
+		time.sleep(timer)
 		i=i+1
+
+def flaskThread():
+	app.run(debug=True, use_reloader=False)
+
 if __name__ == '__main__':
+	thread.start_new_thread(flaskThread,())
 	main()
 	sys.exit(0)
