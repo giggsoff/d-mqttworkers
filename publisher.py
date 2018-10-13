069b8f6f08ca229ea20377a7e8fe8511d6b83cc6
Peter Fedchenkov <giggsoff@gmail.com>
2018-10-13 20:26:50 +0300

    initial subscribe publish

---
+++ publisher.py
@@ -0,0 +1,31 @@
+
+import ssl
+import sys
+
+import paho.mqtt.client
+import paho.mqtt.publish
+
+def on_connect(client, userdata, flags, rc):
+	print('connected')
+
+def main():
+	paho.mqtt.publish.single(
+		topic='topic1',
+		payload='message1',
+		qos=2,
+		hostname='127.0.0.1',
+		port=1883,
+		client_id='client2',
+		#auth={
+		#	'username': '[username]',
+		#	'password': '[password]'
+		#},
+		#tls={
+		#	'ca_certs': '/etc/ssl/certs/DST_Root_CA_X3.pem',
+		#	'tls_version': ssl.PROTOCOL_TLSv1_2
+		#}
+	)
+
+if __name__ == '__main__':
+	main()
+	sys.exit(0)
---
+++ subscriber.py
@@ -0,0 +1,27 @@
+import ssl
+import sys
+
+import paho.mqtt.client
+
+def on_connect(client, userdata, flags, rc):
+	print('connected (%s)' % client._client_id)
+	client.subscribe(topic='topic1', qos=2)
+
+def on_message(client, userdata, message):
+	print('------------------------------')
+	print('topic: %s' % message.topic)
+	print('payload: %s' % message.payload)
+	print('qos: %d' % message.qos)
+
+def main():
+	client = paho.mqtt.client.Client(client_id='client1', clean_session=False)
+	#client.username_pw_set('[username]', '[password]')
+	client.on_connect = on_connect
+	client.on_message = on_message
+	#client.tls_set('/etc/ssl/certs/DST_Root_CA_X3.pem', tls_version=ssl.PROTOCOL_TLSv1_2)
+	client.connect(host='127.0.0.1', port=1883)
+	client.loop_forever()
+
+if __name__ == '__main__':
+	main()
+	sys.exit(0)
